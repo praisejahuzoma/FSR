e22594cacdf4e4addc44d0fa19fbfd45e4d2df82
praisejah uzoma <praisejah.c.uzoma@gmail.com>
2023-08-18 15:36:58 +0100

    update

--- main.js
+++ main.js
@@ -7,15 +7,27 @@ const totalBooksElement = document.getElementById("totalBooks");
 
 let records = [];
 
-// Function to update the local storage
-function updateLocalStorage() {
-  localStorage.setItem("records", JSON.stringify(records));
-}
 const map = L.map('map').setView([0, 0], 13);
 L.tileLayer('https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png', {
   attribution: '&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
 }).addTo(map);
 
+// Reverse geocoding function
+function reverseGeocode(lat, lon) {
+  fetch(`https://nominatim.openstreetmap.org/reverse?lat=${lat}&lon=${lon}&format=json`)
+    .then(response => response.json())
+    .then(data => {
+      if (data.address) {
+        const address = `${data.address.road}, ${data.address.city}, ${data.address.country}`;
+        alert(`Reverse Geocoded Address: ${address}`);
+      } else {
+        alert('Address not found.');
+      }
+    })
+    .catch(error => {
+      console.error('Error reverse geocoding:', error);
+    });
+}
 
 const getAddressButton = document.getElementById('getAddressButton');
 
@@ -29,6 +41,10 @@ getAddressButton.addEventListener('click', () => {
           const location = data[0];
           const lat = parseFloat(location.lat);
           const lon = parseFloat(location.lon);
+
+          // Call reverse geocoding function
+          reverseGeocode(lat, lon);
+
           map.setView([lat, lon], 15);
           L.marker([lat, lon]).addTo(map).bindPopup(address).openPopup();
         } else {
@@ -40,23 +56,38 @@ getAddressButton.addEventListener('click', () => {
       });
   }
 });
+// ... Rest of your existing code ...
+
 
 const getCurrentLocationButton = document.getElementById('getCurrentLocationButton');
 
 getCurrentLocationButton.addEventListener('click', () => {
   if (navigator.geolocation) {
     navigator.geolocation.getCurrentPosition(
-      position => {
+      async position => {
         const lat = position.coords.latitude;
         const lon = position.coords.longitude;
-        const address = `Latitude: ${lat}, Longitude: ${lon}`;
 
-        // Update the address input field with the current location
-        document.getElementById('address').value = address;
+        try {
+          const response = await fetch(`https://nominatim.openstreetmap.org/reverse?format=json&lat=${lat}&lon=${lon}`);
+          const data = await response.json();
 
-        // Set the map view to the current location
-        map.setView([lat, lon], 15);
-        L.marker([lat, lon]).addTo(map).bindPopup(address).openPopup();
+          if (data.display_name) {
+            const address = data.display_name;
+
+            // Update the address input field with the current location
+            document.getElementById('address').value = address;
+
+            // Set the map view to the current location
+            map.setView([lat, lon], 15);
+            L.marker([lat, lon]).addTo(map).bindPopup(address).openPopup();
+          } else {
+            alert('Address not found.');
+          }
+        } catch (error) {
+          console.error('Error fetching address:', error);
+          alert('Error fetching address. Please try again.');
+        }
       },
       error => {
         console.error('Error getting current location:', error);
@@ -67,6 +98,10 @@ getCurrentLocationButton.addEventListener('click', () => {
     alert('Geolocation is not supported by your browser.');
   }
 });
+
+function updateLocalStorage() {
+  localStorage.setItem('records', JSON.stringify(records));
+}
 
 function deleteRecord(button) {
   const li = button.parentElement;
@@ -82,8 +117,43 @@ function deleteRecord(button) {
   // Update totals
   updateTotals();
 
+  // Update local storage
   updateLocalStorage();
+
+  // Delete the record from the server
+  deleteRecordFromServer(record);
 }
+
+
+
+function deleteRecordFromServer(record) {
+  fetch(`http://localhost:3000/deleteRecord`, {
+    method: 'DELETE', // Change this line
+    headers: {
+      'Content-Type': 'application/json',
+    },
+    body: JSON.stringify(record),
+  })
+    .then(response => response.json())
+    .then(data => {
+      console.log('Record deleted from server:', data);
+    })
+    .catch(error => {
+      console.error('Error deleting record from server:', error);
+    });
+}
+
+
+function loadLocalStorage() {
+  const storedRecords = localStorage.getItem('records');
+  if (storedRecords) {
+    records = JSON.parse(storedRecords);
+    loadRecords(); // Update the UI with loaded records
+  }
+}
+
+// Call this function when the page loads
+loadLocalStorage();
 
 function loadRecords() {
   recordList.innerHTML = "";
@@ -104,7 +174,6 @@ function loadRecords() {
       <span>Books: ${record.books}</span>
       <span>Videos: ${record.videos}</span>
       <span>Address: ${record.address}</span>
-      <span>Householder: ${record.householder}</span>
       <span>Name: ${record.name}</span>
       <span>Book Given: ${record.book}</span>
       <button class="delete-button">Delete</button>
@@ -152,6 +221,39 @@ function updateTotals() {
   totalBooksElement.textContent = totalBooks;
 }
 
+function addRecordToServer(newRecord) {
+  fetch('http://localhost:3000/addRecord', {
+    method: 'POST',
+    headers: {
+      'Content-Type': 'application/json',
+    },
+    body: JSON.stringify(newRecord),
+  })
+    .then(response => response.json())
+    .then(data => {
+      console.log('Record added:', data);
+      // Reload records from the server
+      fetchRecords();
+    })
+    .catch(error => {
+      console.error('Error adding record:', error);
+    });
+}
+
+function loadRecordsFromServer() {
+  fetch('http://localhost:3000/getRecords')
+    .then(response => response.json())
+    .then(data => {
+      records = data; // Update the local records array with fetched data
+      loadRecords(); // Update the UI with loaded records
+    })
+    .catch(error => {
+      console.error('Error fetching records:', error);
+    });
+}
+
+fetchRecords();
+
 function addRecord() {
   const date = document.getElementById("date").value;
   const returnVisit = parseFloat(document.getElementById("returnVisit").value) || 0;
@@ -176,12 +278,7 @@ function addRecord() {
     book,
   };
 
-  // Add the new record to the local array
-  records.push(newRecord);
-
-  // Update the UI and totals
-  loadRecords();
-  updateLocalStorage();
+ 
 
   // Clear input fields
   document.getElementById("date").value = "";
@@ -193,19 +290,23 @@ function addRecord() {
   document.getElementById("address").value = "";
   document.getElementById("name").value = "";
   document.getElementById("book").value = "";
-}
 
+  records.push(newRecord);
+
+  // Update the UI with the new record
+  loadRecords();
+
+  // Add the new record to the server
+  addRecordToServer(newRecord);
+ 
+}
 
 
 function fetchRecords() {
-  const storedRecords = localStorage.getItem("records");
-  if (storedRecords) {
-    records = JSON.parse(storedRecords);
-    loadRecords();
-  }
+  loadRecordsFromServer();
 }
 
-// Load records from local storage when the page loads
+// Load records from the server when the page loads
 fetchRecords();
 
 // Add event listener to the Add Record button
