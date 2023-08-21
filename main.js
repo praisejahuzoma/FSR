5ab22afab50c2c9533516356e895e516d4edad5d
praisejah uzoma <praisejah.c.uzoma@gmail.com>
2023-08-21 10:51:27 +0100

    site update

--- main.js
+++ main.js
@@ -1,142 +1,247 @@
-import * as L from 'leaflet';
+const recordList = document.getElementById("recordList");
+const totalHoursElement = document.getElementById("totalHours");
+const totalVideosElement = document.getElementById("totalVideos");
+const totalReturnVisitElement = document.getElementById("totalReturnVisit");
+const totalBibleStudyElement = document.getElementById("totalBibleStudy");
+const totalBooksElement = document.getElementById("totalBooks");
 
-function initializeMap() {
-  const map = L.map('map').setView([0, 0], 13);
-  L.tileLayer('https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png', {
-    attribution: '&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
-  }).addTo(map);
+let records = [];
+
+const map = L.map('map').setView([0, 0], 13);
+L.tileLayer('https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png', {
+  attribution: '&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
+}).addTo(map);
+
+// Reverse geocoding function
+function reverseGeocode(lat, lon) {
+  fetch(`https://nominatim.openstreetmap.org/reverse?lat=${lat}&lon=${lon}&format=json`)
+    .then(response => response.json())
+    .then(data => {
+      console.log(data); // Log the data object to the console
+      if (data.address) {
+        const address = `${data.address.road}, ${data.address.city}, ${data.address.country}`;
+        alert(`Reverse Geocoded Address: ${address}`);
+      } else {
+        alert('Address not found.');
+      }
+    })
+    .catch(error => {
+      console.error('Error reverse geocoding:', error);
+    });
 }
 
+const getAddressButton = document.getElementById('getAddressButton');
 
-document.addEventListener("DOMContentLoaded", () => {
-  const recordList = document.getElementById("recordList");
-  const totalHoursElement = document.getElementById("totalHours");
-  const totalVideosElement = document.getElementById("totalVideos");
-  const totalReturnVisitElement = document.getElementById("totalReturnVisit");
-  const totalBibleStudyElement = document.getElementById("totalBibleStudy");
-  const totalBooksElement = document.getElementById("totalBooks");
+getAddressButton.addEventListener('click', () => {
+  const address = prompt('Enter the address:');
+  if (address) {
+    fetch(`https://nominatim.openstreetmap.org/search?q=${encodeURIComponent(address)}&format=json`)
+      .then(response => response.json())
+      .then(data => {
+        if (data.length > 0) {
+          const location = data[0];
+          const lat = parseFloat(location.lat);
+          const lon = parseFloat(location.lon);
 
-  let records = [];
+          // Call reverse geocoding function
+          reverseGeocode(lat, lon);
 
-  const map = L.map('map').setView([0, 0], 13);
-  L.tileLayer('https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png', {
-    attribution: '&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
-  }).addTo(map);
+          map.setView([lat, lon], 15);
+          L.marker([lat, lon]).addTo(map).bindPopup(address).openPopup();
+        } else {
+          alert('Address not found.');
+        }
+      })
+      .catch(error => {
+        console.error('Error fetching address:', error);
+      });
+  }
+});
+
+const getCurrentLocationButton = document.getElementById('getCurrentLocationButton');
+
+getCurrentLocationButton.addEventListener('click', () => {
+  if (navigator.geolocation) {
+    navigator.geolocation.getCurrentPosition(
+      async position => {
+        const lat = position.coords.latitude;
+        const lon = position.coords.longitude;
+
+        try {
+          const response = await fetch(`https://nominatim.openstreetmap.org/reverse?format=json&lat=${lat}&lon=${lon}`);
+          const data = await response.json();
+
+          if (data.display_name) {
+            const address = data.display_name;
+
+            // Update the address input field with the current location
+            document.getElementById('address').value = address;
+
+            // Set the map view to the current location
+            map.setView([lat, lon], 15);
+            L.marker([lat, lon]).addTo(map).bindPopup(address).openPopup();
+          } else {
+            alert('Address not found.');
+          }
+        } catch (error) {
+          console.error('Error fetching address:', error);
+          alert('Error fetching address. Please try again.');
+        }
+      },
+      error => {
+        console.error('Error getting current location:', error);
+        alert('Error getting current location. Please try again.');
+      }
+    );
+  } else {
+    alert('Geolocation is not supported by your browser.');
+  }
+});
+
+function updateLocalStorage() {
+  localStorage.setItem('records', JSON.stringify(records));
+}
+
+function deleteRecord(button) {
+  const li = button.parentElement;
+  const index = Array.from(recordList.children).indexOf(li);
+  const record = records[index];
+
+  // Remove from UI
+  recordList.removeChild(li);
+
+  // Remove from local records array
+  records.splice(index, 1);
+
+  // Update totals
+  updateTotals();
+
+  // Update local storage
+  updateLocalStorage();
+}
+
+function loadLocalStorage() {
+  const storedRecords = localStorage.getItem('records');
+  if (storedRecords) {
+    records = JSON.parse(storedRecords);
+    loadRecords(); // Update the UI with loaded records
+  }
+}
+
+// Call this function when the page loads
+loadLocalStorage();
+
+function loadRecords() {
+  recordList.innerHTML = "";
+
+  let totalHours = 0;
+  let totalVideos = 0;
+  let totalReturnVisit = 0;
+  let totalBibleStudy = 0;
+  let totalBooks = 0;
+
+  for (const record of records) {
+    const li = document.createElement("li");
+    li.innerHTML = `
+      <span>Date: ${record.date}</span>
+      <span>Return Visit: ${record.returnVisit}</span>
+      <span>Bible Study: ${record.bibleStudy}</span>
+      <span>Hour: ${record.hour}</span>
+      <span>Books: ${record.books}</span>
+      <span>Videos: ${record.videos}</span>
+      <span>Address: ${record.address}</span>
+      <span>Name: ${record.name}</span>
+      <span>Book Given: ${record.book}</span>
+      <button class="delete-button">Delete</button>
+    `;
+
+    recordList.appendChild(li);
+
+    totalHours += record.hour;
+    totalVideos += record.videos;
+    totalReturnVisit += record.returnVisit;
+    totalBibleStudy += record.bibleStudy;
+    totalBooks += record.books;
+  }
+
+  updateTotals(totalHours, totalVideos, totalReturnVisit, totalBibleStudy, totalBooks);
+
+  // Add event listeners to the delete buttons
+  const deleteButtons = document.querySelectorAll(".delete-button");
+  deleteButtons.forEach(button => {
+    button.addEventListener("click", () => deleteRecord(button));
+  });
+}
+
+function updateTotals() {
+  let totalHours = 0;
+  let totalVideos = 0;
+  let totalReturnVisit = 0;
+  let totalBibleStudy = 0;
+  let totalBooks = 0;
+
+  for (const record of records) {
+    totalHours += record.hour;
+    totalVideos += record.videos;
+    totalReturnVisit += record.returnVisit;
+    totalBibleStudy += record.bibleStudy;
+    totalBooks += record.books;
+  }
+
+  totalHoursElement.textContent = totalHours.toFixed(2);
+  totalVideosElement.textContent = totalVideos;
+  totalReturnVisitElement.textContent = totalReturnVisit;
+  totalBibleStudyElement.textContent = totalBibleStudy;
+  totalBooksElement.textContent = totalBooks;
+}
+
+function addRecord() {
+  const date = document.getElementById("date").value;
+  const returnVisit = parseFloat(document.getElementById("returnVisit").value) || 0;
+  const bibleStudy = parseFloat(document.getElementById("bibleStudy").value) || 0;
+  const hour = parseFloat(document.getElementById("hour").value) || 0;
+  const books = parseFloat(document.getElementById("books").value) || 0;
+  const videos = parseFloat(document.getElementById("videos").value) || 0;
+
+  const address = document.getElementById("address").value;
+  const name = document.getElementById("name").value;
+  const book = document.getElementById("book").value;
+
+  const newRecord = {
+    date,
+    returnVisit,
+    bibleStudy,
+    hour,
+    books,
+    videos,
+    address,
+    name,
+    book,
+  };
 
  
 
-  const googleMapsScript = document.createElement('script');
-googleMapsScript.src = `https://maps.googleapis.com/maps/api/js?key=AIzaSyBs-THNeMB_GwhroQ7ELQP38alB4q_Lva8&libraries=places`;
-googleMapsScript.onload = initializeMap;
-document.head.appendChild(googleMapsScript);
+  // Clear input fields
+  document.getElementById("date").value = "";
+  document.getElementById("returnVisit").value = "";
+  document.getElementById("bibleStudy").value = "";
+  document.getElementById("hour").value = "";
+  document.getElementById("books").value = "";
+  document.getElementById("videos").value = "";
+  document.getElementById("address").value = "";
+  document.getElementById("name").value = "";
+  document.getElementById("book").value = "";
 
-  const geocoder = L.Control.geocoder({
-    defaultMarkGeocode: false,
-    placeholder: "Enter the address..."
-  }).on('markgeocode', e => {
-    const lat = e.geocode.center.lat;
-    const lon = e.geocode.center.lng;
-    reverseGeocode(lat, lon);
-    map.setView([lat, lon], 15);
-    L.marker([lat, lon]).addTo(map).bindPopup(e.geocode.name).openPopup();
-  }).addTo(map);
+  records.push(newRecord);
 
-  const getCurrentLocationButton = document.getElementById('getCurrentLocationButton');
-  getCurrentLocationButton.addEventListener('click', () => {
-    if (navigator.geolocation) {
-      navigator.geolocation.getCurrentPosition(
-        async position => {
-          const lat = position.coords.latitude;
-          const lon = position.coords.longitude;
+  // Update the UI with the new record
+  loadRecords();
 
-          try {
-            const response = await fetch(`https://nominatim.openstreetmap.org/reverse?format=json&lat=${lat}&lon=${lon}`);
-            const data = await response.json();
+  // Add the new record to the server
+ 
+ 
+}
 
-            if (data.display_name) {
-              const address = data.display_name;
-
-              document.getElementById('address').value = address;
-
-              map.setView([lat, lon], 15);
-              L.marker([lat, lon]).addTo(map).bindPopup(address).openPopup();
-            } else {
-              alert('Address not found.');
-            }
-          } catch (error) {
-            console.error('Error fetching address:', error);
-            alert('Error fetching address. Please try again.');
-          }
-        },
-        error => {
-          console.error('Error getting current location:', error);
-          alert('Error getting current location. Please try again.');
-        }
-      );
-    } else {
-      alert('Geolocation is not supported by your browser.');
-    }
-  });
-
-  function reverseGeocode(lat, lon) {
-    const latLng = new google.maps.LatLng(lat, lon);
-
-    geocoder.geocode({ location: latLng }, (results, status) => {
-      if (status === "OK" && results[0]) {
-        const address = results[0].formatted_address;
-        alert(`Reverse Geocoded Address: ${address}`);
-      } else {
-        alert("Address not found.");
-      }
-    });
-  }
-
-  // ... (continue with the rest of your code)
-
-   function addRecord() {
-    const date = document.getElementById("date").value;
-    const returnVisit = parseFloat(document.getElementById("returnVisit").value) || 0;
-    const bibleStudy = parseFloat(document.getElementById("bibleStudy").value) || 0;
-    const hour = parseFloat(document.getElementById("hour").value) || 0;
-    const books = parseFloat(document.getElementById("books").value) || 0;
-    const videos = parseFloat(document.getElementById("videos").value) || 0;
-
-    const address = document.getElementById("address").value;
-    const name = document.getElementById("name").value;
-    const book = document.getElementById("book").value;
-
-    const newRecord = {
-      date,
-      returnVisit,
-      bibleStudy,
-      hour,
-      books,
-      videos,
-      address,
-      name,
-      book,
-    };
-
-    // Clear input fields
-    document.getElementById("date").value = "";
-    document.getElementById("returnVisit").value = "";
-    document.getElementById("bibleStudy").value = "";
-    document.getElementById("hour").value = "";
-    document.getElementById("books").value = "";
-    document.getElementById("videos").value = "";
-    document.getElementById("address").value = "";
-    document.getElementById("name").value = "";
-    document.getElementById("book").value = "";
-
-    records.push(newRecord);
-
-    // Update the UI with the new record
-    loadRecords();
-  }
-
-  // Add event listener to the Add Record button
-  const addRecordButton = document.getElementById("addRecordButton");
-  addRecordButton.addEventListener("click", addRecord);
-
-  // ... (continue with the rest of your code)
-});
+// Add event listener to the Add Record button
+const addRecordButton = document.getElementById("addRecordButton");
+addRecordButton.addEventListener("click", addRecord);
